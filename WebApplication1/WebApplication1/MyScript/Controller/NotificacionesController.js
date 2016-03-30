3be0aa58be4ad172832ed00c2f9bd1bc62c5b3f2
Carlos Arzuza <carlosarzuzaquiroz@gmail.com>
2016-03-30 08:54:42 -0500

    Notificaciones Listo

--- WebApplication1/WebApplication1/MyScript/Controller/NotificacionesController.js
+++ WebApplication1/WebApplication1/MyScript/Controller/NotificacionesController.js
@@ -2,8 +2,8 @@
     $scope.Poliza = {}; //Objeto Actual
     $scope.Polizas = []; //Listado de Objetos
 
- 
 
+    
     inicialice();
 
     loadRecordPolizas();
@@ -26,28 +26,48 @@
         promiseGet.then(function (pl) {
             $scope.Polizas = pl.data;
 
-            angular.forEach($scope.Polizas, function (item, dato) {
+            var miArray = new Array($scope.Polizas.length);
+            console.log($scope.Polizas.length);
+            /*
+            for (i = 0; i < $scope.Polizas.length; i++) {
                 fecha = new Date();
                 day = fecha.getDate();
                 month = fecha.getMonth() + 1;
                 year = fecha.getFullYear();
 
-                var d = new Date(month + " " + day + " " + year);
+                var hoy = new Date(month + " " + day + " " + year);
 
-                console.log(d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear());
-                fecha = new Date(item.FECHA_FINAL_POL);
-                console.log(fecha.getDate() + "/" + fecha.getMonth() + "/" + fecha.getFullYear())
-                dif = fecha.getTime() - d.getTime();
-                var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
-                console.log(dias);
+                fecha = new Date($scope.Polizas[i].FECHA_FINAL_POL);
 
-                if (dias <= 30) {
-                    $scope.Notificacion = "OK";
-                    console.log($scope.Notificacion)
-                }else{
-                    $scope.Notificacion = "NO";
-                    console.log($scope.Notificacion)
+                dif = fecha.getTime() - hoy.getTime();
+                $scope.dias = Math.floor(dif / (1000 * 60 * 60 * 24));
+                console.log($scope.dias);
+
+
+
+                miArray[i] = $scope.dias;
+
+                if (miArray[i] <= 30) {
+                    console.log("entro")
+                    $scope.myStyle = { 'color': 'blue' }
                 }
+            }
+            */
+            console.log(miArray);
+
+            angular.forEach($scope.Polizas, function (item, dato) {
+                fecha = new Date();
+                day = fecha.getDate();
+                month = fecha.getMonth() + 1;
+                year = fecha.getFullYear();
+                
+                var hoy = new Date(month + " " + day + " " + year);
+
+                fecha = new Date(item.FECHA_FINAL_POL);
+
+                dif = fecha.getTime() - hoy.getTime();
+                $scope.dias = Math.floor(dif / (1000 * 60 * 60 * 24));
+                console.log($scope.dias);
                 
             });
             //console.log($scope.Polizas);
@@ -57,69 +77,5 @@
            });
     }
 
-    
-
-
-
-
-
-    /*
-    function restaFechas(f1, f2) {
-        var aFecha1 = f1.split('/');
-        var aFecha2 = f2.split('/');
-        var fFecha1 = Date.UTC(aFecha1[2], aFecha1[1] - 1, aFecha1[0]);
-        var fFecha2 = Date.UTC(aFecha2[2], aFecha2[1] - 1, aFecha2[0]);
-        console.log("fFecha1 " + fFecha1)
-        console.log("fFecha2 " + fFecha2)
-        var dif = fFecha2 - fFecha1;
-        var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
-        return dias;
-    }
-
-    fecha = new Date();
-    day = fecha.getDate();
-    month = fecha.getMonth() + 1;
-    year = fecha.getFullYear();
-
-    console.log("Fecha actual: " + day + "/" + month + "/" + year);
-
-    var f1 = day + "/" + month + "/" + year;
-    var f2 = item.FECHA_FINAL_POL;
-    //var f2 = '28/3/2016';
-
-    console.log("fecha " + f2);
-    console.log("resta " + restaFechas(f1, f2));
-    */
-    /*
-    function restar(days) {
-        
-        fecha = new Date();
-        day = fecha.getDate();
-        month = fecha.getMonth() + 1;
-        year = fecha.getFullYear();
-
-        console.log("Fecha actual: " + day + "/" + month + "/" + year);
-
-
-        tiempo = fecha.getTime();
-        milisegundos = parseInt(days * 24 * 60 * 60 * 1000);
-        total = fecha.setTime(tiempo + milisegundos);
-        day = fecha.getDate();
-        month = fecha.getMonth() + 1;
-        year = fecha.getFullYear();
-
-        console.log("Fecha modificada: " + day + "/" + month + "/" + year);
-
-    }
-    restar(-45);
-    
-    console.log(d.getTime())
-    fecha = new Date("03/31/2016");
-    console.log(fecha.getTime())
-    dif = fecha.getTime() - d.getTime();
-    var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
-    console.log(dias)
-    
-    */
 
 });
